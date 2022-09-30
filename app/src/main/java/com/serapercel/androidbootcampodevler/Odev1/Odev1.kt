d16201f1ef15d2c37338b6afcb7ebbcd62885b1c
Serap <ercelserap@gmail.com>
2022-09-30 16:56:42 +0300

    Add Homework 1

---
+++ app/src/main/java/com/serapercel/androidbootcampodevler/Odev1/Odev1.kt
@@ -0,0 +1,32 @@
+package com.serapercel.kotlindersleri.degisken_olusturma
+
+fun main() {
+
+    //  islem
+    val islemNo: Int = 1
+    val ogrNo: Int = 281
+    val kitapNo: Int = 2
+    val aTarih: String = "26.09.2022"
+    val vTarih: String = "27.09.2022"
+
+    //  ogrenci
+    val ogrAd: String = "Serap"
+    val ogrSoyad: String = "Erçel"
+    val cinsiyet: String = "Kadın"
+    val dTarih: String = "01.01.2000"
+    val sinif: String = "Mezun"
+
+    //  kitap
+    val isbNo: Int = 2345
+    val yazarNo: Int = 23
+    val turNo: Int = 41
+    val sayfaSayisi: Int = 345
+    val puan: Int = 10
+
+    //  yazar
+    val yazarAd: String = "Yuval Noah"
+    val yazarSoyad: String = "Harari"
+
+    //  tur
+    val turAdi: String = "Felsefe"
+}
